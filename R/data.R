6fc1d804d75d48fc78075b5acef1ea5c6a08e8e4
Ragnhild Ørbæk Laursen <au543194@uni.au.dk>
2022-05-02 14:06:51 +0200

    add data folder

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Mutational counts for 21 breast cancer patients
+#'
+#' A dataset containing the mutational counts of 96 different mutation types
+#' for 21 breast cancer patients.
+#'
+#'
+#' @format A data matrix with 96 rows and 21 columns:
+#'
+#' @source \url{http://www.findUrl.dk}
+"BRCA21"
